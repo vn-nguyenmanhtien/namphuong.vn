76ddf5bd2010d8338a704f5d47c337de85047139
NguyenManhTien <vn.nguyenmanhtien@gmail.com>
2025-03-10 18:44:29 +0700

    update intro

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -1,13 +1,13 @@
 // @/api/site/getSite.ts
  const siteData = {
         id: 1,
-        name: "Professional Nextjs Travel booking theme",
-        url: "https://nexttravel.joombooking.com",
+        name: "Nam Phương tận hưởng không gian sống",
+        url: "https://namphuong.vercel.app",
         params: JSON.stringify({
-            meta_title: "Next Travel - Explore the World with Seamless Booking Experiences",
-            meta_description: "Next Travel offers curated travel experiences, exclusive deals on flights, hotels, and tour packages. Book your dream vacation effortlessly.",
-            meta_keywords: "travel, booking, tours, vacation, flights, hotels, travel deals",
-            tagline: "Discover. Book. Travel.",
+            meta_title: "Nam Phương Chuyên Thiết kế & Thi công Sân vườn, Hồ cá Koi chuyên nghiệp",
+            meta_description: "Nam Phương là đơn vị hàng đầu trong lĩnh vực thiết kế, thi công và bảo trì cảnh quan thương mại, mang đến những giải pháp không gian xanh hiện đại và bền vững. Với nhiều năm kinh nghiệm, chúng tôi tự hào cung cấp các dịch vụ chất lượng cao cho các doanh nghiệp, khu đô thị, trung tâm thương mại, khách sạn, khu nghỉ dưỡng và nhiều công trình công cộng khác.",
+            meta_keywords: "Thiết kế - Sân Vườn, Tiểu cảnh",
+            tagline: "Thiết kế - thi công. Sân Vườn. Tiểu cảnh.",
             
         })
  };
